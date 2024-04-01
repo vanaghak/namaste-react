2642c58954efb1f88b46948609b6f3e0cac1c027
PrajwalPrashanth <vanaghak286@gmail.com>
2024-04-01 15:43:57 +0530

    adding changes to correct author

--- app.js
+++ app.js
@@ -13,23 +13,23 @@
  * 
  * ReactElement(object) => HTML (Browser Understands)
  * */
-  
- const parent = React.createElement("div", {id:"parent"},[
-    React.createElement("div", {id:"child"},[
+   
+ const parent = React.createElement("div", {id:"parent"},[ 
+    React.createElement("div", {id:"child"},[ 
         
-      React.createElement("h1",{},"i'm an h1 tag"),
-      React.createElement("h2",{},"i'm an h2 tag"),
-    ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"i'm an h1 tag"),
-        React.createElement("h2",{},"i'm an h2 tag"),
-    ]),
-]);
+      React.createElement("h1",{},"i'm an h1 tag"), 
+      React.createElement("h2",{},"i'm an h2 tag"), 
+    ]), 
+    React.createElement("div",{id:"child2"},[ 
+        React.createElement("h1",{},"i'm an h1 tag"), 
+        React.createElement("h2",{},"i'm an h2 tag"), 
+    ]), 
+]); 
+  
+    console.log(parent); 
  
-    console.log(parent);
-
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-
-    root.render(parent);
+    const root = ReactDOM.createRoot(document.getElementById("root")); 
+ 
+    root.render(parent); 
 
     
